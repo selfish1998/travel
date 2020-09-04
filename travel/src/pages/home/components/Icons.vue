5feb9d95b2dd41d24f306e3932dcf8d1ec8991f8
huangdian <752998845@qq.com>
2020-09-04 10:40:25 +0800

    finish icons

---
+++ travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="icons">
+    <swiper >
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" :alt="item.imgAlt">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png',
+          imgAlt: '景点门票',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+          imgAlt: '周边游hot',
+          desc: '周边游hot'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
+          imgAlt: '德天瀑布',
+          desc: '德天瀑布'
+        },
+        {
+          id: '0004',
+          imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png',
+          imgAlt: '景点门票',
+          desc: '景点门票'
+        },
+        {
+          id: '0005',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+          imgAlt: '周边游hot',
+          desc: '周边游hot'
+        },
+        {
+          id: '0006',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
+          imgAlt: '德天瀑布',
+          desc: '德天瀑布'
+        },
+        {
+          id: '0007',
+          imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png',
+          imgAlt: '景点门票',
+          desc: '景点门票'
+        },
+        {
+          id: '0008',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+          imgAlt: '周边游hot',
+          desc: '周边游hot'
+        },
+        {
+          id: '0009',
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+          imgAlt: '泡温泉',
+          desc: '泡温泉'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height 0
+    padding-bottom 50%
+  .icon
+    position relative
+    width 25%
+    float left
+    height 0
+    overflow hidden
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        height 100%
+        margin 0 auto
+    .icon-desc
+      position absolute
+      bottom 0
+      left 0
+      right 0
+      height  .44rem
+      line-height .44rem
+      color $darkTextColor
+      text-align center
+      ellipsis()
+</style>
--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -20,13 +20,13 @@ export default {
       swiperList: [
         {
           id: 0,
-          imgUrl: 'https://imgs.qunarzz.com/sight/p0/201407/30/b71784bdf6afa1a4f28dfb3467fe0253.jpg_640x276_7f51ed27.jpg',
-          imgAlt: '独秀峰王城景区'
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1507/9f/f8dde5c968f8ac.jpg_r_640x214_83194a9b.jpg',
+          imgAlt: '广西最美瀑布'
         },
         {
           id: 1,
-          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1512/fe/fe08d17ffe7d036b90.water.jpg_640x276_b7d4f2b7.jpg',
-          imgAlt: '龙脊梯田'
+          imgUrl: 'https://imgs.qunarzz.com/sight/source/1507/12/0f76178631aa56.jpg_r_640x214_b3adf11d.jpg',
+          imgAlt: '广西水上乐园哪家强'
         }
       ]
     }
@@ -43,7 +43,7 @@ export default {
     width 100%
     height 0
     overflow hidden
-    padding-bottom 43.125%
+    padding-bottom 37.6761%
     background-color #eee
     .swpier-img
       width 100%
