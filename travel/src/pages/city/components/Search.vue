1db1df32eaafdcd4e4607242fc3948b6aa05c8ab
huangdian <752998845@qq.com>
2020-09-23 17:12:07 +0800

    finish city-search-logic

--- travel/src/pages/city/components/Search.vue
+++ travel/src/pages/city/components/Search.vue
@@ -1,11 +1,66 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  <div>
+    <div class="search">
+        <input v-model="keyWord" class="search-input" type="text" placeholder="输入城市名或拼音"/>
+      </div>
+      <div class="search-content" ref="search" v-show="keyWord">
+        <ul>
+          <li v-for="(item) of list" :key="item.id" class="search-item">
+            {{item.name}}
+          </li>
+          <li class="search-item border-bottom" v-show="hasNodata">
+            没有找到匹配数据
+          </li>
+        </ul>
+      </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
+
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyWord: '',
+      list: [],
+      timer: null
+    }
+  },
+  updated () {
+    this.scroll = new Bscroll(this.$refs.search)
+    console.log(this.scroll)
+  },
+  computed: {
+    hasNodata () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyWord () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyWord) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyWord) > -1 || value.name.indexOf(this.keyWord) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -23,4 +78,18 @@ export default {
       border-radius .06rem
       color #666
       padding 0 .1rem
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background #fff
+      color #666
 </style>
