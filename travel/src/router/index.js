0f7df10793c5a6c5d95cfed4950c5625024d19ce
huangdian <752998845@qq.com>
2020-09-27 12:15:21 +0800

    finish common-gallary

--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -2,15 +2,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/city'
+import Detail from '@/pages/detail/detail'
 
 Vue.use(Router)
+// push
+const VueRouterPush = Router.prototype.push
+Router.prototype.push = function push (to) {
+  return VueRouterPush.call(this, to).catch(err => err)
+}
 
+// replace
+const VueRouterReplace = Router.prototype.replace
+Router.prototype.replace = function replace (to) {
+  return VueRouterReplace.call(this, to).catch(err => err)
+}
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: Detail
     },
     {
       path: '/city',
